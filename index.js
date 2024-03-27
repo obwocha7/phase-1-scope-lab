b3222825a14ebc36443fee3997d4a6c981531495
Derrick <omoriderrick@gmail.com>
2024-03-27 10:09:02 +0300

    New Feat: functions scope globally

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+const leastFavoriteCustomer = `jack`;
+var customerName = `bob`;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = `maybe bob`;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = `mary`;
+}
